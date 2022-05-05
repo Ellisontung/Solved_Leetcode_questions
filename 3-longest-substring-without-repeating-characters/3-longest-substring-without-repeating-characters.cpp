8d4d17befcd4d9696334670df159d424678ba533
Yen Cheng Tung <103092066+Ellisontung@users.noreply.github.com>
2022-05-05 18:41:44 -0400

    Time: 0 ms (100.00%), Space: 7.6 MB (85.21%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,9 +4,9 @@ public:
     //two pointers move with the same directions and do not overtake each other.
     //Time complexity: O(n)
     int lengthOfLongestSubstring(string s) {
-        // vector<int> map(128,0);
-        unordered_map<char,int> map;
-        for (auto c : s) map[c] = 0;
+        vector<int> map(128,0);
+        // unordered_map<char,int> map;
+        // for (auto c : s) map[c] = 0;
         int counter=0, begin=0, end=0, d=0; 
         while(end<s.size()){
             if(map[s[end++]]++>0) counter++; 
