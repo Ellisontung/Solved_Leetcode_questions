d68aac8d3249aec5eff7d6ec0ccf4bf110729649
Yen Cheng Tung <103092066+Ellisontung@users.noreply.github.com>
2022-05-10 15:29:02 -0400

    Time: 60 ms (30.14%), Space: 10.1 MB (49.48%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> stack;
+    vector<vector<int>> combine(int n, int k) {
+        backtrack(0,0,n,k);
+        return res;
+    }
+    void backtrack(int start,int num,int n, int k){
+        //Push a bigger number in a combine vector every time.
+        //Pop the least num and push a bigger num.
+        //if size == k push to result vector.
+        if(stack.size()==k){
+            res.push_back(stack);
+        }
+        
+        if(stack.size()<k){
+            for(int i = start ; i < n ;i++){
+                stack.push_back(i+1);
+                backtrack(i+1,num+1,n,k);
+                stack.pop_back();
+            }
+        }
+    }
+};
