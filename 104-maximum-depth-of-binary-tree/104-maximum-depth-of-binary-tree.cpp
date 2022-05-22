55075e70bf411d4c408bcbf1fa96ddd455eab652
Yen Cheng Tung <103092066+Ellisontung@users.noreply.github.com>
2022-05-21 21:22:19 -0400

    Time: 19 ms (17.18%), Space: 18.9 MB (14.40%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -11,10 +11,34 @@
  */
 class Solution {
 public:
-        int maxDepth(TreeNode* root) {
+    int maxDepth(TreeNode* root) {
+        // DFS
+        //=========
         if(root == NULL) return 0;
         else{
             return max(maxDepth(root->left),maxDepth(root->right))+1;
         }
+
+        //BFS
+        //=========
+        // if(root==NULL) return 0;
+        // queue<TreeNode *> q;
+        // q.push(root);
+        // int level = 0;
+        // while(!q.empty()){
+        //     ++ level;
+        //     for(int i = 0 ; i < q.size() ; ++i){
+        //         TreeNode* temp = q.front();
+        //         q.pop();
+        //         if(temp->left!=NULL){
+        //             q.push(temp->left);
+        //         }
+        //         if(temp->right!=NULL){
+        //             q.push(temp->right);
+        //         }
+        //     }
+        // }
+        // return level;
     }
+    
 };
