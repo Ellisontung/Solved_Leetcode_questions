b661cdad9084081dd15485b08fca3612825c4ff0
Yen Cheng Tung <103092066+Ellisontung@users.noreply.github.com>
2022-07-11 15:07:52 -0400

    Time: 375 ms (50.02%), Space: 10.4 MB (87.67%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -8,14 +8,11 @@ public:
         for (int i = nums.size()-1 ; i > -1 ; i--){
             for(int j = i ; j < nums.size() ; j++){
                 if(nums[i]<nums[j]){
-                    // cout<<nums[i]<<"\t"<<nums[j]<<endl;
                     LIS[i]=max(LIS[i], 1 + LIS[j]);
                 }
             }
         }
-        // for(int i = 0 ; i <nums.size() ; i++){
-        //     cout<<LIS[i]<<" ";
-        // }
+
         return *max_element(LIS , LIS+ nums.size()-1);
     }
 };
