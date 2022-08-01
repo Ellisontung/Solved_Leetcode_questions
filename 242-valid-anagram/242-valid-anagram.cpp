09f6532c2ee5306b4b0e4031eb24b22f884fb108
Yen Cheng Tung <103092066+Ellisontung@users.noreply.github.com>
2022-08-01 13:06:38 -0400

    Time: 16 ms (48.53%), Space: 7.4 MB (7.16%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> mp1,mp2;
-        for(int i = 0 ; i < s.size() ; i++){
-            mp1[s[i]]++;
-        }
-        for(int j = 0 ; j < t.size() ; j++){
-            mp2[t[j]]++;
-        }
-        return mp1==mp2;
+        // unordered_map<char,int> mp1,mp2;
+        // for(int i = 0 ; i < s.size() ; i++){
+        //     mp1[s[i]]++;
+        // }
+        // for(int j = 0 ; j < t.size() ; j++){
+        //     mp2[t[j]]++;
+        // }
+        // return mp1==mp2;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
